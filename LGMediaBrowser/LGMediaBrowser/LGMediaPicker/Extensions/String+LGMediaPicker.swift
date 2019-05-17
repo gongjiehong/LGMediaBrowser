67ce75ad7a7c7d069a61585c8545a8a54b2b9817
龚杰洪 <gjhlovelh@vip.qq.com>
2019-05-17 18:32:00 +0800

    更新高度计算相关内容

--- LGMediaBrowser/LGMediaBrowser/LGMediaPicker/Extensions/String+LGMediaPicker.swift
+++ LGMediaBrowser/LGMediaBrowser/LGMediaPicker/Extensions/String+LGMediaPicker.swift
@@ -11,19 +11,28 @@ import Foundation
 extension String {
     public func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = NSLineBreakMode.byWordWrapping
+        
         let boundingBox = self.boundingRect(with: constraintRect,
                                             options: .usesLineFragmentOrigin,
-                                            attributes: [NSAttributedString.Key.font: font],
+                                            attributes: [NSAttributedString.Key.font: font,
+                                                         NSAttributedString.Key.paragraphStyle: paragraphStyle],
                                             context: nil)
-        
-        return ceil(boundingBox.height)
+        return ceil(boundingBox.height) + abs(boundingBox.origin.y)
     }
     
     public func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = NSLineBreakMode.byWordWrapping
+        
         let boundingBox = self.boundingRect(with: constraintRect,
                                             options: .usesLineFragmentOrigin,
-                                            attributes: [NSAttributedString.Key.font: font],
+                                            attributes: [NSAttributedString.Key.font: font,
+                                                         NSAttributedString.Key.paragraphStyle: paragraphStyle],
                                             context: nil)
         
         return ceil(boundingBox.width)
